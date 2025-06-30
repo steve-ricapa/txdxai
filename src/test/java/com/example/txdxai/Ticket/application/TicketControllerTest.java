55a00caf2d495d21dc31b3ea058672073975a835
steve-ricapa <steve.ricapa@gmail.com>
2025-06-30 15:24:17 -0500

    Test omit

--- src/test/java/com/example/txdxai/Ticket/application/TicketControllerTest.java
+++ src/test/java/com/example/txdxai/Ticket/application/TicketControllerTest.java
@@ -14,7 +14,6 @@ import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
--- src/test/java/com/example/txdxai/User/application/UserControllerTest.java
+++ src/test/java/com/example/txdxai/User/application/UserControllerTest.java
@@ -13,7 +13,6 @@ import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
